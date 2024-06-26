86ee31dbdea1a6614da2c8368000c00eb3c1fb45
polka-dotcom <navyaonline7@gmail.com>
2024-06-26 22:12:47 +0400

    added main operation function

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function divide(a, b) {
     return a / b; 
 }
 
+let num1; 
+let num2; 
+let op; 
+
+function operate() {
+    if (op == "+") {
+        add(num1, num2); 
+    } else if (op == "-") {
+        subtract(num1,num2);
+    } else if (op == "*") {
+        multiply(num1, num2);
+    } else if (op == "/") {
+        divide(num1, num2);
+    } else {
+        alert("ERROR!"); 
+    }
+}
